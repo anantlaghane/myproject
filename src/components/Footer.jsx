6463e500617cdce88a69f760331953209f1eb619
anantlaghane <anantlaghane.gmail.com>
2024-03-11 14:38:22 +0530

    -m

---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+
+export default function Footer() {
+    
+    return (
+        <div style={{padding: "12px 0px", backgroundColor : "rgb(232, 227, 227)", textAlign: 'center', position:'fixed', width: '100%',height : '85px', bottom : '0', left: '0', right: '0', zIndex : '999'}}>
+            
+            <div className="text-center">
+                    <h5>
+                        Made with ❤️ by <a href="https://anant-laghane-vg0a.onrender.com/" style={{textDecoration : "none", color: "red"}}>Anant Laghane</a>
+                    </h5>
+            </div> 
+
+            <div className="text-center pt-1">
+                <a href="https:linkedin.com/in/anant-laghane-76738b100" target="_blank" rel="noreferrer">
+                    <i className="bi bi-linkedin mx-2" style={{fontSize : "20px"}}></i>
+                </a>
+                
+                <a href="https://anant-laghane-vg0a.onrender.com/" target="_blank" rel="noreferrer">
+                    <i className="bi bi-globe mx-2" style={{fontSize : "20px"}}></i>
+                </a>
+
+                <a href="https://github.com/AnantLaghane" target="_blank" rel="noreferrer">
+                    <i className="bi bi-github mx-2" style={{fontSize : "21px"}}></i>
+                </a>
+
+                <a href="mailto:anantlaghane@gmail.com" target="_blank" rel="noreferrer">
+                    <i className="bi bi-envelope-fill mx-2" style={{fontSize : "21px"}}></i>
+                </a>
+            </div>
+            {/* <footer style={{backgroundColor: 'crimson', color:'springgreen', textAlign: 'center', position:'fixed', width: '100%',height : '50px', bottom : '0', left: '0', right: '0', zIndex : '999'}}><h1>THIS IS MY FOOTER</h1></footer> */}
+        </div>
+       
+    )
+}
